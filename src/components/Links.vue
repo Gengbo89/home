963556b44da37443ce5ed9abcb8143b52de20cc5
Gengbo89 <2604213923@qq.com>
2025-12-10 16:22:38 +0800

    update hitokot api

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, Compass, CompactDisc, PhotoVideo } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Compass, CompactDisc, PhotoVideo, CommentAlt } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -66,7 +66,8 @@ const siteIcon = {
   Blog,
   Compass,
   CompactDisc,
-  PhotoVideo
+  PhotoVideo,
+  CommentAlt
 };
 
 // 链接跳转
