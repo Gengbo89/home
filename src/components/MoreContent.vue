0e4ad5e864ed858eb5447646b9d421327d367786
Gengbo89 <2604213923@qq.com>
2025-12-12 18:16:04 +0800

    fix some issues

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+    <div>时光如水不回头，</div>
+    <div>浮生长梦几春秋。</div>
+    <div>朝来新露添香色，</div>
+    <div>暮去残阳染寺楼。</div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .more-content {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   margin-top: 20px;
