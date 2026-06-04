a37bacb9b3ab0c68dac14f2c0fc5ce6a42ad151c
Gengbo89 <2604213923@qq.com>
2026-06-04 14:11:12 +0800

    update link

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,6 +44,7 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, Compass, CompactDisc, PhotoVideo, CommentAlt, Cloud } from "@vicons/fa"; // 注意使用正确的类别
+import { CloudMonitoring, Notebook, Document } from "@vicons/carbon"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -68,7 +69,10 @@ const siteIcon = {
   CompactDisc,
   PhotoVideo,
   CommentAlt,
-  Cloud
+  Cloud,
+  CloudMonitoring,
+  Notebook,
+  Document
 };
 
 // 链接跳转
